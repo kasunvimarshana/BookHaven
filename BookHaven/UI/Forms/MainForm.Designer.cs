bfc4941d2694716aefd39edbf122f59d708a2b72
kasunvimarshana <kasunvmail@gmail.com>
2025-03-17 04:24:14 +0530

    add chart widgets

--- BookHaven/UI/Forms/MainForm.Designer.cs
+++ BookHaven/UI/Forms/MainForm.Designer.cs
@@ -35,18 +35,33 @@
             btnOrderManager = new Button();
             btnSaleManager = new Button();
             panel1 = new Panel();
-            lblName = new Label();
-            labelName = new Label();
-            labelRole = new Label();
             labelEmail = new Label();
             lblRole = new Label();
             lblEmail = new Label();
+            labelRole = new Label();
+            labelName = new Label();
+            lblName = new Label();
+            splitContainer1 = new SplitContainer();
+            panelMonthlySales = new Panel();
+            panelProductSales = new Panel();
+            panelRevenueByGenre = new Panel();
+            panelTopSellingBooks = new Panel();
+            panelOrderStatus = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // btnBookManager
             // 
-            btnBookManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnBookManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnBookManager.Location = new Point(12, 185);
             btnBookManager.Name = "btnBookManager";
             btnBookManager.Size = new Size(235, 85);
@@ -57,7 +72,7 @@
             // 
             // btnCustomerManager
             // 
-            btnCustomerManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnCustomerManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnCustomerManager.Location = new Point(276, 185);
             btnCustomerManager.Name = "btnCustomerManager";
             btnCustomerManager.Size = new Size(235, 85);
@@ -68,7 +83,7 @@
             // 
             // btnSupplierManager
             // 
-            btnSupplierManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnSupplierManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnSupplierManager.Location = new Point(12, 276);
             btnSupplierManager.Name = "btnSupplierManager";
             btnSupplierManager.Size = new Size(235, 85);
@@ -79,7 +94,7 @@
             // 
             // btnUserManager
             // 
-            btnUserManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnUserManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnUserManager.Location = new Point(276, 276);
             btnUserManager.Name = "btnUserManager";
             btnUserManager.Size = new Size(235, 85);
@@ -90,7 +105,7 @@
             // 
             // btnOrderManager
             // 
-            btnOrderManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnOrderManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnOrderManager.Location = new Point(12, 367);
             btnOrderManager.Name = "btnOrderManager";
             btnOrderManager.Size = new Size(235, 85);
@@ -101,7 +116,7 @@
             // 
             // btnSaleManager
             // 
-            btnSaleManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold);
+            btnSaleManager.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Bold, GraphicsUnit.Point);
             btnSaleManager.Location = new Point(276, 367);
             btnSaleManager.Name = "btnSaleManager";
             btnSaleManager.Size = new Size(235, 85);
@@ -124,40 +139,10 @@
             panel1.Size = new Size(499, 167);
             panel1.TabIndex = 6;
             // 
-            // lblName
-            // 
-            lblName.AutoSize = true;
-            lblName.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            lblName.Location = new Point(13, 10);
-            lblName.Name = "lblName";
-            lblName.Size = new Size(68, 28);
-            lblName.TabIndex = 0;
-            lblName.Text = "Name";
-            // 
-            // labelName
-            // 
-            labelName.AutoSize = true;
-            labelName.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            labelName.Location = new Point(156, 10);
-            labelName.Name = "labelName";
-            labelName.Size = new Size(58, 28);
-            labelName.TabIndex = 1;
-            labelName.Text = "-NA-";
-            // 
-            // labelRole
-            // 
-            labelRole.AutoSize = true;
-            labelRole.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            labelRole.Location = new Point(156, 117);
-            labelRole.Name = "labelRole";
-            labelRole.Size = new Size(58, 28);
-            labelRole.TabIndex = 2;
-            labelRole.Text = "-NA-";
-            // 
             // labelEmail
             // 
             labelEmail.AutoSize = true;
-            labelEmail.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            labelEmail.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
             labelEmail.Location = new Point(156, 62);
             labelEmail.Name = "labelEmail";
             labelEmail.Size = new Size(58, 28);
@@ -167,7 +152,7 @@
             // lblRole
             // 
             lblRole.AutoSize = true;
-            lblRole.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblRole.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
             lblRole.Location = new Point(13, 117);
             lblRole.Name = "lblRole";
             lblRole.Size = new Size(54, 28);
@@ -177,18 +162,153 @@
             // lblEmail
             // 
             lblEmail.AutoSize = true;
-            lblEmail.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblEmail.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
             lblEmail.Location = new Point(13, 62);
             lblEmail.Name = "lblEmail";
             lblEmail.Size = new Size(64, 28);
             lblEmail.TabIndex = 3;
             lblEmail.Text = "Email";
             // 
+            // labelRole
+            // 
+            labelRole.AutoSize = true;
+            labelRole.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            labelRole.Location = new Point(156, 117);
+            labelRole.Name = "labelRole";
+            labelRole.Size = new Size(58, 28);
+            labelRole.TabIndex = 2;
+            labelRole.Text = "-NA-";
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            labelName.Location = new Point(156, 10);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(58, 28);
+            labelName.TabIndex = 1;
+            labelName.Text = "-NA-";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lblName.Location = new Point(13, 10);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(68, 28);
+            lblName.TabIndex = 0;
+            lblName.Text = "Name";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(517, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(panelMonthlySales);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(panelProductSales);
+            splitContainer1.Size = new Size(696, 440);
+            splitContainer1.SplitterDistance = 348;
+            splitContainer1.TabIndex = 7;
+            // 
+            // panelMonthlySales
+            // 
+            panelMonthlySales.Location = new Point(3, 31);
+            panelMonthlySales.Name = "panelMonthlySales";
+            panelMonthlySales.Size = new Size(342, 406);
+            panelMonthlySales.TabIndex = 0;
+            // 
+            // panelProductSales
+            // 
+            panelProductSales.Location = new Point(3, 31);
+            panelProductSales.Name = "panelProductSales";
+            panelProductSales.Size = new Size(338, 406);
+            panelProductSales.TabIndex = 0;
+            // 
+            // panelRevenueByGenre
+            // 
+            panelRevenueByGenre.Location = new Point(12, 501);
+            panelRevenueByGenre.Name = "panelRevenueByGenre";
+            panelRevenueByGenre.Size = new Size(499, 356);
+            panelRevenueByGenre.TabIndex = 8;
+            // 
+            // panelTopSellingBooks
+            // 
+            panelTopSellingBooks.Location = new Point(520, 501);
+            panelTopSellingBooks.Name = "panelTopSellingBooks";
+            panelTopSellingBooks.Size = new Size(342, 356);
+            panelTopSellingBooks.TabIndex = 9;
+            // 
+            // panelOrderStatus
+            // 
+            panelOrderStatus.Location = new Point(872, 501);
+            panelOrderStatus.Name = "panelOrderStatus";
+            panelOrderStatus.Size = new Size(341, 356);
+            panelOrderStatus.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Monthly Sales";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Book Sales";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 473);
+            label3.Name = "label3";
+            label3.Size = new Size(153, 25);
+            label3.TabIndex = 10;
+            label3.Text = "Revenue By Genre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(520, 473);
+            label4.Name = "label4";
+            label4.Size = new Size(152, 25);
+            label4.TabIndex = 11;
+            label4.Text = "Top Selling Books";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(872, 473);
+            label5.Name = "label5";
+            label5.Size = new Size(111, 25);
+            label5.TabIndex = 12;
+            label5.Text = "Order Status";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(524, 476);
+            ClientSize = new Size(1225, 869);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(panelOrderStatus);
+            Controls.Add(panelTopSellingBooks);
+            Controls.Add(panelRevenueByGenre);
+            Controls.Add(splitContainer1);
             Controls.Add(panel1);
             Controls.Add(btnSaleManager);
             Controls.Add(btnOrderManager);
@@ -201,7 +321,14 @@
             Load += MainForm_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -219,5 +346,16 @@
         private Label labelEmail;
         private Label lblRole;
         private Label lblEmail;
+        private SplitContainer splitContainer1;
+        private Panel panelMonthlySales;
+        private Panel panelProductSales;
+        private Panel panelRevenueByGenre;
+        private Panel panelTopSellingBooks;
+        private Panel panelOrderStatus;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
--- BookHaven/UI/Forms/MainForm.cs
+++ BookHaven/UI/Forms/MainForm.cs
@@ -8,18 +8,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BookHaven.UI.Forms;
+using Models = BookHaven.Models;
 using BookHaven.Helpers;
 using BookHaven.Enums;
-using Models = BookHaven.Models;
+using BookHaven.BLL;
+using LiveChartsCore; // Add LiveChartsCore namespace
+using LiveChartsCore.SkiaSharpView; // Add SkiaSharpView namespace
+using LiveChartsCore.SkiaSharpView.WinForms; // Add WinForms namespace
+using LiveChartsCore.SkiaSharpView.Painting; // Add Painting namespace
+using SkiaSharp; // Add SkiaSharp namespace
 
 namespace BookHaven.UI.Forms
 {
     public partial class MainForm : Form
     {
+        private readonly SalesService _salesService;
+        private readonly SalesDetailService _salesDetailService;
+        private readonly OrderService _orderService;
+        private readonly BookService _bookService;
         Models.User? _currentUser;
+
+        private CartesianChart monthlySalesChart; // Chart for monthly sales
+        private CartesianChart productSalesChart; // Chart for product sales
+        private PieChart revenueByGenreChart; // Chart for revenue by genre
+        private CartesianChart topSellingBooksChart; // Chart for top-selling books
+        private PieChart orderStatusChart; // Chart for order status distribution
+
         public MainForm()
         {
             InitializeComponent();
+            _salesService = new SalesService();
+            _salesDetailService = new SalesDetailService();
+            _orderService = new OrderService();
+            _bookService = new BookService();
+
+            // Initialize LiveChartsCore charts
+            monthlySalesChart = new CartesianChart { Dock = DockStyle.Fill };
+            productSalesChart = new CartesianChart { Dock = DockStyle.Fill };
+            revenueByGenreChart = new PieChart { Dock = DockStyle.Fill };
+            topSellingBooksChart = new CartesianChart { Dock = DockStyle.Fill };
+            orderStatusChart = new PieChart { Dock = DockStyle.Fill };
+
+            // Add charts to the form
+            panelMonthlySales.Controls.Add(monthlySalesChart);
+            panelProductSales.Controls.Add(productSalesChart);
+            panelRevenueByGenre.Controls.Add(revenueByGenreChart);
+            panelTopSellingBooks.Controls.Add(topSellingBooksChart);
+            panelOrderStatus.Controls.Add(orderStatusChart);
+
+            // Load dashboard data
+            LoadDashboardData();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -46,6 +84,211 @@ namespace BookHaven.UI.Forms
             btnOrderManager.Visible = LoggedInUserSession.HasRole(UserRole.Admin);
             btnSaleManager.Visible = LoggedInUserSession.HasRole(UserRole.Admin) || LoggedInUserSession.HasRole(UserRole.SalesClerk);
         }
+
+        private List<decimal> GetMonthlySales(List<Models.Sale> sales)
+        {
+            var monthlySales = new List<decimal>();
+            var months = Enum.GetValues(typeof(Month)).Cast<Month>(); // Using the Month enum to loop through months
+
+            // Initialize the monthly sales list with 0s for each month
+            foreach (var month in months)
+            {
+                decimal monthlyTotal = 0;
+                foreach (var sale in sales)
+                {
+                    if (sale.SaleDate.Month == (int)month) // Checking if the sale's month matches the current month
+                    {
+                        monthlyTotal += sale.TotalAmount;
+                    }
+                }
+                monthlySales.Add(monthlyTotal); // Add the total sales for this month to the list
+            }
+
+            return monthlySales;
+        }
+
+        private Dictionary<string, int> GetProductSales(List<Models.Sale> sales)
+        {
+            var productSales = new Dictionary<string, int>();
+
+            foreach (var sale in sales)
+            {
+                sale.SalesDetails = _salesDetailService.GetSalesDetailBySaleId(sale.Id);
+                foreach (var detail in sale.SalesDetails)
+                {
+                    Models.Book? book = _bookService.GetBookById(detail.BookId);
+                    if (book != null)
+                    {
+                        if (!productSales.ContainsKey(book.Title))
+                        {
+                            productSales[book.Title] = 0;
+                        }
+                        productSales[book.Title] += detail.Quantity;
+                    }
+                }
+            }
+
+            return productSales;
+        }
+
+        private Dictionary<string, decimal> GetRevenueByGenre(List<Models.Sale> sales)
+        {
+            var revenueByGenre = sales
+                .SelectMany(s => s.SalesDetails)
+                .GroupBy(d => _bookService.GetBookById(d.BookId)?.Genre)
+                .ToDictionary(g => g.Key, g => g.Sum(d => d.Price * d.Quantity));
+
+            return revenueByGenre;
+        }
+
+        private Dictionary<string, int> GetTopSellingBooks(List<Models.Sale> sales)
+        {
+            var topSellingBooks = sales
+                .SelectMany(s => s.SalesDetails)
+                .GroupBy(d => _bookService.GetBookById(d.BookId)?.Title)
+                .ToDictionary(g => g.Key, g => g.Sum(d => d.Quantity))
+                .OrderByDescending(kvp => kvp.Value)
+                .Take(10) // Top 10 books
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return topSellingBooks;
+        }
+
+        private Dictionary<string, int> GetOrderStatusDistribution(List<Models.Order> orders)
+        {
+            var orderStatusDistribution = orders
+                .GroupBy(o => o.OrderStatus.ToString())
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            return orderStatusDistribution;
+        }
+
+        //
+        private void LoadDashboardData()
+        {
+            try
+            {
+                // Fetching data
+                var sales = _salesService.GetAllSales();
+                var orders = _orderService.GetAllOrders();
+
+                // Aggregating data
+                var monthlySales = GetMonthlySales(sales);
+                var productSales = GetProductSales(sales);
+                var revenueByGenre = GetRevenueByGenre(sales);
+                var topSellingBooks = GetTopSellingBooks(sales);
+                var orderStatusDistribution = GetOrderStatusDistribution(orders);
+
+                /////
+                //// Line Chart for Monthly Sales
+                //monthlySalesChart.Series = new ISeries[]
+                //{
+                //    new LineSeries<decimal>
+                //    {
+                //        Values = monthlySales,
+                //        Name = "Monthly Sales",
+                //        Stroke = new SolidColorPaint(SKColors.Blue, 2), // Customize line color and thickness
+                //        Fill = null // No fill under the line
+                //    }
+                //};
+
+                //// Bar Chart for Product-wise Sales
+                //productSalesChart.Series = new ISeries[]
+                //{
+                //    new ColumnSeries<int>
+                //    {
+                //        Values = productSales.Values,
+                //        Name = "Product Sales",
+                //        Fill = new SolidColorPaint(SKColors.Green) // Customize bar color
+                //    }
+                //};
+
+                //// Set X-axis labels for product sales chart
+                //productSalesChart.XAxes = new List<Axis>
+                //{
+                //    new Axis
+                //    {
+                //        Labels = productSales.Keys.ToList(),
+                //        Name = "Products"
+                //    }
+                //};
+                /////
+                // Line Chart for Monthly Sales
+                monthlySalesChart.Series = new ISeries[]
+                {
+                    new LineSeries<decimal>
+                    {
+                        Values = monthlySales,
+                        Name = "Monthly Sales",
+                        Stroke = new SolidColorPaint(SKColors.Blue, 2), // Customize line color and thickness
+                        Fill = null // No fill under the line
+                    }
+                };
+
+                // Bar Chart for Product-wise Sales
+                productSalesChart.Series = new ISeries[]
+                {
+                    new ColumnSeries<int>
+                    {
+                        Values = productSales.Values,
+                        Name = "Product Sales",
+                        Fill = new SolidColorPaint(SKColors.Green) // Customize bar color
+                    }
+                };
+
+                // Pie Chart for Revenue by Genre
+                revenueByGenreChart.Series = revenueByGenre
+                    .Select(kvp => new PieSeries<decimal>
+                    {
+                        Values = new List<decimal> { kvp.Value },
+                        Name = kvp.Key,
+                        //DataLabels = true
+                    })
+                    .Cast<ISeries>()
+                    .ToList();
+
+                // Bar Chart for Top-Selling Books
+                topSellingBooksChart.Series = new ISeries[]
+                {
+                    new ColumnSeries<int>
+                    {
+                        Values = topSellingBooks.Values,
+                        Name = "Top-Selling Books",
+                        Fill = new SolidColorPaint(SKColors.Orange)
+                    }
+                };
+
+                topSellingBooksChart.XAxes = new List<Axis>
+                {
+                    new Axis { Labels = topSellingBooks.Keys.ToList(), Name = "Books" }
+                };
+
+                // Pie Chart for Order Status Distribution
+                orderStatusChart.Series = orderStatusDistribution
+                    .Select(kvp => new PieSeries<int>
+                    {
+                        Values = new List<int> { kvp.Value },
+                        Name = kvp.Key,
+                        //DataLabels = true
+                    })
+                    .Cast<ISeries>()
+                    .ToList();
+
+
+                // Update total sales label
+                //totalSalesLabel.Text = $"Total Sales: {sales.Count}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Error loading dashboard data: {ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+        //
 
         private void btnBookManager_Click(object sender, EventArgs e)
         {
@@ -82,5 +325,11 @@ namespace BookHaven.UI.Forms
             Sale.ManageSalesForm manageSalesForm = new Sale.ManageSalesForm();
             manageSalesForm.Show();
         }
+
+        private void cartesianChart1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
